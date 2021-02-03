ab49327f62f3efc4b73274181c736ba358055083
Danilo Paulino da Silva <danilopaulinodasilva@gmail.com>
2021-02-03 09:23:33 -0300

    add controller

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 
 const routes = express.Router();
+const TweetController = require("./controllers/TweetController");
 
-routes.get("/", (req,res) => {
-    return res.send("hellow");
-
-});
+routes.get("/tweets", TweetController.index);
+routes.post("/tweets", TweetController.store);
 
 module.exports = routes;
