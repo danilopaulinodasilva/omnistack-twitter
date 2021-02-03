6ff5de387f70e7c90fa959cb1687789deed9942a
Danilo Paulino da Silva <danilopaulinodasilva@gmail.com>
2021-02-03 09:07:54 -0300

    add dependencies; created model; created base folder and files;

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb+srv://danilopaulinodasilva:KuuUw5A4Jo8ykMIb@cluster0.cu8ew.mongodb.net/omnistack-twitter?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.get('/', (req,res) => {
+    return res.send("Hello world!")
+
+});
+
+app.listen(3000, () => {
+    console.log("Server started at port 3000");  
+});
